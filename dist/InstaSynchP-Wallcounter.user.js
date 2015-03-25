d157f6d6b638a0642a8ae4fb91ef76920b8fb793
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-03-25 16:54:52 +0100

    v1.0.1

--- dist/InstaSynchP-Wallcounter.user.js
+++ dist/InstaSynchP-Wallcounter.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Summarizes the lengths of each users video walls
 
-// @version     1.0.0
+// @version     1.0.1
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Wallcounter
 // @license     MIT
@@ -16,11 +16,41 @@
 // @require     https://greasyfork.org/scripts/5647-instasynchp-library/code/InstaSynchP%20Library.js?version=37716
 // ==/UserScript==
 
+function Wall(username) {
+  "use strict";
+  this.duration = 0;
+  this.videoCount = 0;
+  this.username = username;
+}
+
+Wall.prototype.add = function (video) {
+  "use strict";
+  this.videoCount++;
+  this.duration += video.duration;
+};
+
+Wall.prototype.remove = function (video) {
+  "use strict";
+  this.videoCount--;
+  this.duration -= video.duration;
+};
+
+Wall.prototype.format = function (format) {
+  "use strict";
+  var th = this;
+  return format.format(
+    th.username,
+    window.utils.secondsToTime(th.duration),
+    th.videoCount
+  );
+};
+
 function Wallcounter() {
   "use strict";
-  this.version = '1.0.0';
+  this.version = '1.0.1';
   this.name = 'InstaSynchP Wallcounter';
   this.counter = {};
+  this.ownCounter = undefined;
   this.commands = {
     "'wallcounter": {
       'hasArguments': true,
@@ -38,48 +68,93 @@ function Wallcounter() {
 
 Wallcounter.prototype.resetVariables = function () {
   "use strict";
+  this.ownCounter = undefined;
   this.counter = {};
+};
+
+Wallcounter.prototype.updateOwnDisplay = function () {
+  "use strict";
+  var th = this;
+  $('#playlist_wallcounter').text(
+    th.ownCounter.format('Wallcounter[{1} - {2}]')
+  );
+};
+
+Wallcounter.prototype.key = function (key) {
+  "use strict";
+  return key.toLowerCase();
+};
+
+Wallcounter.prototype.increase = function (username, video) {
+  "use strict";
+  this.counter[this.key(username)].add(video);
+};
+
+Wallcounter.prototype.decrease = function (username, video) {
+  "use strict";
+  this.counter[this.key(username)].remove(video);
+};
+
+Wallcounter.prototype.create = function (username) {
+  "use strict";
+  this.counter[this.key(username)] = new Wall(username);
+};
+
+Wallcounter.prototype.createIfNotExists = function (username) {
+  "use strict";
+  var th = this;
+  if (th.counter.hasOwnProperty(th.key(username))) {
+    return;
+  }
+  th.create(username);
+};
+
+Wallcounter.prototype.bindUpdates = function () {
+  "use strict";
+  var th = this;
+
+  function onAddVideo(video) {
+    th.increase(video.addedby, video);
+    th.updateOwnDisplay();
+  }
+
+  function onAddUser(user) {
+    th.createIfNotExists(user.username);
+  }
+
+  //LoadPlaylist happens before LoadUserlist so the walls would not have been
+  //created yet when recieving the AddVideo events
+  events.on(th, 'LoadPlaylist', function (videos) {
+    videos.forEach(function (video) {
+      onAddUser({
+        username: video.addedby
+      });
+      onAddVideo(video);
+    });
+    events.on(th, "AddVideo", onAddVideo, true);
+  });
+
+  events.on(th, "AddUser", onAddUser);
+
+  events.on(th, "RemoveVideo", function (ignore, video) {
+    th.decrease(video.addedby, video);
+    th.updateOwnDisplay();
+  }, true);
 };
 
 Wallcounter.prototype.executeOnce = function () {
   "use strict";
   var th = this;
-
-  function count(video, duration, inc) {
-    var name = video.addedby.toLowerCase();
-    if (th.counter.hasOwnProperty(name)) {
-      //update wallcounter
-      th.counter[name].duration += duration;
-      th.counter[name].count += inc;
-    } else {
-      //create wallcounter
-      th.counter[name] = {
-        duration: duration,
-        count: 1,
-        origName: video.addedby
-      };
-    }
-    //update own wallcounter
-    if (name === thisUser().username.toLowerCase()) {
-      $('#playlist_wallcounter').text(
-        'Wallcounter[{1} - {0}]'.format(th.counter[name].count,
-          window.utils.secondsToTime(th.counter[name].duration))
-      );
-    }
-  }
-
-  events.on(th, "AddVideo", function (video) {
-    count(video, video.duration, +1);
-  }, true);
-
-  events.on(th, "RemoveVideo", function (ignore, video) {
-    count(video, -video.duration, -1);
-  }, true);
+  th.bindUpdates();
+  events.on(th, 'Joined', function () {
+    th.ownCounter = new Wall(thisUser().username);
+    th.counter[th.key(th.ownCounter.username)] = th.ownCounter;
+  });
 };
 
 Wallcounter.prototype.preConnect = function () {
   "use strict";
-  //add own wallcounter below the playlist
+  //own wallcounter display below the playlist
   $('.playlist-stats').append(
     $('<div>', {
       id: 'playlist_wallcounter'
@@ -87,16 +162,12 @@ Wallcounter.prototype.preConnect = function () {
   );
 };
 
-//let other plugins overwrite this
 Wallcounter.prototype.formatOutput = function (counts) {
   "use strict";
+  //REFACTOR
   var output = "Wallcounter<br>";
   counts.forEach(function (count, index) {
-    output += "{0}[<b>{2}</b> - {1}] - ".format(
-      count.origName,
-      count.count,
-      window.utils.secondsToTime(count.duration)
-    );
+    output += count.format('{0}[<b>{1}</b> - {2}] - ');
     //2 counters per line
     if ((index + 1) % 2 === 0) {
       //remove " - "
@@ -111,36 +182,35 @@ Wallcounter.prototype.formatOutput = function (counts) {
   return output;
 };
 
+Wallcounter.prototype.getWallsForUsernames = function (usernames) {
+  "use strict";
+  var th = this,
+    walls = [];
+  usernames.forEach(function (username) {
+    if (th.counter.hasOwnProperty(th.key(username)) &&
+      th.counter[th.key(username)].videoCount !== 0) {
+      walls.push(th.counter[th.key(username)]);
+    }
+  });
+  return walls;
+};
+
 Wallcounter.prototype.execute = function (opts) {
   "use strict";
   var th = this,
-    list = [];
+    walls = [];
 
-  function addToList(username) {
-    username = username.toLowerCase();
-    if (!th.counter.hasOwnProperty(username) ||
-      th.counter[username].count === 0) {
-      return;
-    }
-    list.push(th.counter[username]);
-  }
-
-  function compareCount(c1, c2) {
-    return c2.duration - c1.duration;
-  }
-
-  //collect users and their durations
   if (opts.usernames.length !== 0) {
-    opts.usernames.forEach(addToList);
+    walls = th.getWallsForUsernames(opts.usernames);
   } else {
-    Object.keys(th.counter).forEach(addToList);
+    walls = th.getWallsForUsernames(Object.keys(th.counter));
   }
 
-  //sort by duration
-  list.sort(compareCount);
+  walls.sort(function (c1, c2) {
+    return c2.duration - c1.duration;
+  });
 
-  //display
-  addSystemMessage(th.formatOutput(list));
+  addSystemMessage(th.formatOutput(walls));
 };
 
 window.plugins = window.plugins || {};
---
+++ tests/test.js
@@ -0,0 +1,266 @@
+//http://joquery.com/2012/string-format-for-javascript
+if (typeof String.prototype.format !== 'function') {
+  String.prototype.format = function () {
+    "use strict";
+    // The string containing the format items (e.g. "{0}")
+    // will and always has to be the first argument.
+    var theString = this,
+      i,
+      regEx;
+
+    // start with the second argument (i = 1)
+    for (i = 0; i < arguments.length; i += 1) {
+      // "gm" = RegEx options for Global search (more than one instance)
+      // and for Multiline search
+      regEx = new RegExp("\\{" + (i) + "\\}", "gm");
+      theString = theString.replace(regEx, arguments[i]);
+    }
+    return theString;
+  };
+}
+
+window.utils = {
+  secondsToTime: function (num) {
+    "use strict";
+    var hours = Math.floor(num / 3600);
+    var minutes = Math.floor((num - (hours * 3600)) / 60);
+    var seconds = num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    var time = "";
+    if (!hours) {
+      time += hours + ':';
+    }
+    time += minutes + ':' + seconds;
+    return time;
+  }
+};
+
+function defaultWallObject(username) {
+  "use strict";
+  return new Wall(username || 'abcde');
+}
+
+function filledWallObject(count, username){
+  "use strict";
+  var wall = defaultWallObject(username);
+  for(var i = 0; i < count; i++){
+    wall.add({
+      duration: 10
+    });
+  }
+  return wall;
+}
+QUnit.module( "Wall" );
+QUnit.test("Create", function (assert) {
+  "use strict";
+  var username = 'abcde',
+    wall = new Wall(username);
+  assert.notEqual(wall, undefined, 'new Object not undefined');
+  assert.strictEqual(wall.duration, 0, 'duration zero default');
+  assert.strictEqual(wall.videoCount, 0, 'videoCount zero default');
+  assert.strictEqual(wall.username, username, 'username set');
+});
+
+QUnit.test("Add", function (assert) {
+  "use strict";
+  var wall = defaultWallObject();
+  wall.add({
+    duration: 10
+  });
+  assert.strictEqual(wall.duration, 10, 'duration increased');
+  assert.strictEqual(wall.videoCount, 1, 'videoCount increased');
+});
+
+QUnit.test("Add several", function (assert) {
+  "use strict";
+  var wall = defaultWallObject();
+  wall.add({
+    duration: 10
+  });
+  wall.add({
+    duration: 10
+  });
+  assert.strictEqual(wall.duration, 20, 'duration increased');
+  assert.strictEqual(wall.videoCount, 2, 'videoCount increased');
+});
+
+QUnit.test("Remove", function (assert) {
+  "use strict";
+  var wall = filledWallObject(1);
+  wall.remove({
+    duration: 10
+  });
+  assert.strictEqual(wall.duration, 0, 'video removed');
+  assert.strictEqual(wall.videoCount, 0, 'videoCount decreased');
+});
+
+QUnit.test("Remove several", function (assert) {
+  "use strict";
+  var wall = filledWallObject(2);
+  wall.remove({
+    duration: 10
+  });
+  wall.remove({
+    duration: 10
+  });
+  assert.strictEqual(wall.duration, 0, 'video removed');
+  assert.strictEqual(wall.videoCount, 0, 'videoCount decreased');
+});
+
+QUnit.test("Format", function (assert) {
+  "use strict";
+  var wall = filledWallObject(2);
+  assert.strictEqual(wall.format('{0} {1} {2}'), 'abcde 00:20 2', 'abcde 00:20 2');
+});
+
+QUnit.module("Wallcounter");
+
+QUnit.test("New object", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  assert.notEqual(wallcounter, undefined, 'new Object not undefined');
+});
+
+QUnit.test("Plugin set", function (assert) {
+  "use strict";
+  assert.strictEqual(window.plugins.wallcounter instanceof Wallcounter, true, 'window.plugins.wallcounter instanceof Wallcounter');
+});
+
+QUnit.test("ResetVariables", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.ownCounter = {};
+  wallcounter.counter = {
+    foo: 'bar'
+  };
+  wallcounter.resetVariables();
+  assert.strictEqual(wallcounter.ownCounter, undefined, 'ownCounter reset');
+  assert.strictEqual(wallcounter.counter.foo, undefined, 'counter reset');
+});
+
+QUnit.test("Key", function (assert) {
+  "use strict";
+  assert.strictEqual(window.plugins.wallcounter.key('ABC'), 'abc', 'Key to lower case');
+});
+
+QUnit.test("Increase", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'abcde': defaultWallObject()
+  };
+  wallcounter.increase('abcde', {duration: 10});
+  assert.strictEqual(wallcounter.counter.abcde.duration, 10, 'Duration incrased');
+  assert.strictEqual(wallcounter.counter.abcde.videoCount, 1, 'VideoCount incrased');
+});
+
+QUnit.test("Increase key test", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'abcde': defaultWallObject()
+  };
+  wallcounter.increase('ABCDE', {duration: 10});
+  assert.strictEqual(wallcounter.counter.abcde.duration, 10, 'Duration incrased');
+  assert.strictEqual(wallcounter.counter.abcde.videoCount, 1, 'VideoCount incrased');
+});
+
+QUnit.test("Decrease", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'abcde': filledWallObject(1)
+  };
+  wallcounter.decrease('abcde', {duration: 10});
+  assert.strictEqual(wallcounter.counter.abcde.duration, 0, 'Duration decreased');
+  assert.strictEqual(wallcounter.counter.abcde.videoCount, 0, 'VideoCount decreased');
+});
+
+QUnit.test("Decrease key test", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'abcde': filledWallObject(1)
+  };
+  wallcounter.decrease('ABCDE', {duration: 10});
+  assert.strictEqual(wallcounter.counter.abcde.duration, 0, 'Duration decreased');
+  assert.strictEqual(wallcounter.counter.abcde.videoCount, 0, 'VideoCount decreased');
+});
+
+QUnit.test("Create", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.create('abcde');
+  assert.strictEqual(wallcounter.counter.abcde instanceof Wall, true, 'New Wall created');
+});
+
+QUnit.test("Create key test", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.create('ABCDE');
+  assert.strictEqual(wallcounter.counter.abcde instanceof Wall, true, 'New Wall created');
+});
+
+QUnit.test("CreateIfNotExists", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.createIfNotExists('abcde');
+  wallcounter.createIfNotExists('ABCDE');
+  assert.strictEqual(wallcounter.counter.abcde.username, 'abcde', 'Wall not overwritten');
+});
+
+QUnit.test("CreateIfNotExists key test", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.createIfNotExists('ABCDE');
+  wallcounter.createIfNotExists('abcde');
+  assert.strictEqual(wallcounter.counter.abcde.username, 'ABCDE', 'Wall not overwritten');
+});
+
+QUnit.test("Create display", function (assert) {
+  "use strict";
+  window.plugins.wallcounter.preConnect();
+  assert.strictEqual($('.playlist-stats')[0].innerHTML,'<div id="playlist_wallcounter">Wallcounter[00:00 - 0]</div>', 'Display created');
+});
+
+QUnit.test("Update display", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+
+  wallcounter.ownCounter = filledWallObject(1);
+  wallcounter.updateOwnDisplay();
+  assert.strictEqual($('#playlist_wallcounter').text(), 'Wallcounter[00:10 - 1]', 'Div updated');
+});
+
+QUnit.test("GetWallsForUsernames", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'foo': filledWallObject(1, 'foo'),
+    'bar': filledWallObject(1, 'bar'),
+    'baz': filledWallObject(1, 'baz')
+  };
+  var walls = wallcounter.getWallsForUsernames(['foo', 'baz']);
+  assert.strictEqual(walls.length, 2, "Two element returned");
+  assert.strictEqual(walls[0].username, 'foo', "First element equal");
+  assert.strictEqual(walls[1].username, 'baz', "Second element equal");
+});
+
+QUnit.test("GetWallsForUsernames key test", function (assert) {
+  "use strict";
+  var wallcounter = new Wallcounter();
+  wallcounter.counter = {
+    'foo': filledWallObject(1, 'foo')
+  };
+  var walls = wallcounter.getWallsForUsernames(['FOO']);
+  assert.strictEqual(walls[0].username, 'foo', "lowercase key");
+});
